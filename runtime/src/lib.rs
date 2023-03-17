95bcf2b27346dc20f85ae11ef03b3834644f2b3b
penumbra23 <glbranimir@gmail.com>
2023-03-17 15:19:49 +0100

    Rename the template pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -48,7 +48,7 @@ pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_nft;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -276,8 +276,8 @@ impl pallet_sudo::Config for Runtime {
 	type RuntimeCall = RuntimeCall;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+/// Configure the pallet-nft in pallets/nft.
+impl pallet_nft::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
@@ -297,8 +297,8 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		// Include the custom logic from the pallet-nft in the runtime.
+		TemplateModule: pallet_nft,
 	}
 );
 
@@ -345,7 +345,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_nft, TemplateModule]
 	);
 }
 
